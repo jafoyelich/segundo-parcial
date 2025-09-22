5e20d222d45f1b7885a7abcd0d7f75443262e681
Yousoro <jafetquiroga@gmail.com>
2025-09-22 03:26:52 -0400

    feat(noti): local notification permission and demo buttons

--- src/app/pages/home.ts
+++ src/app/pages/home.ts
@@ -10,5 +10,30 @@ import {RouterModule} from '@angular/router';
   styleUrls: ['./home.scss']
 })
 export class Home {
+  pedirPermiso() {
+    if (!('Notification' in window)) {
+      alert('Este navegador no soporta notificaciones.');
+      return;
+    }
+
+    Notification.requestPermission().then((perm) => {
+      if (perm === 'granted') {
+        alert('Permiso concedido. Ya puedes recibir notificaciones.');
+      } else {
+        alert('Permiso denegado.');
+      }
+    });
+  }
+
+  enviarNotificacion() {
+    if (Notification.permission === 'granted') {
+      new Notification('🎉 Notificación de prueba', {
+        body: 'Esto es una notificación enviada desde Angular.',
+        icon: 'assets/icons/icon-192x192.png'
+      });
+    } else {
+      alert('Debes permitir las notificaciones primero.');
+    }
+  }
 
 }
